c0e45ef0b0992f3d05d1d3d699c1634e4884cb8c
georgetheking123 <101723439+georgetheking123@users.noreply.github.com>
2022-03-16 21:08:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/nfcs/add/:id', db.addNfc)
 app.get('/nfcs', db.getNfcs)
 app.get('/nfcs/:id', db.getNfcStatusById)
 app.patch('/nfcs/:id', db.updateSwipedNfc)
-app.get('/nfcs/reset', db.updateAllNfcs)
+app.get('/reset/nfcs', db.updateAllNfcs)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
