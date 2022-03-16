0d14102659b2b708e6072e271bd3c7c11efc4286
georgetheking123 <101723439+georgetheking123@users.noreply.github.com>
2022-03-16 16:41:29 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 })
 
 const getNfcs = (request, response) => {
