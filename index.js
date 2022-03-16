d21e2c1b69cb79469d364b42fff42787938276ff
georgetheking123 <101723439+georgetheking123@users.noreply.github.com>
2022-03-16 15:48:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries');
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/nfcs', db.getNfcs)
+app.get('/nfcs/:id', db.getNfcStatusById)
+app.patch('/nfcs/:id', db.updateSwipedNfc)
+app.patch('/nfcs', db.updateAllNfcs)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,56 @@
+const Pool = require('pg').Pool
+require("dotenv").config();
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+})
+
+const getNfcs = (request, response) => {
+    pool.query('SELECT * FROM nfcs', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+const getNfcStatusById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query(`SELECT was_swiped FROM nfcs WHERE nfc_id = '${id}'`, (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+const updateSwipedNfc = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query(`UPDATE nfcs SET was_swiped = true WHERE nfc_id = '${id}'`, (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200)
+    })
+}
+
+const updateAllNfcs = (request, response) => {
+    pool.query(`UPDATE nfcs SET was_swiped = false`, (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200)
+    })
+}
+
+module.exports = {
+    getNfcs,
+    getNfcStatusById,
+    updateSwipedNfc,
+    updateAllNfcs
+};
